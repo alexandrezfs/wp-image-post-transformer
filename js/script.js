e14857157cdac6ff89ce665c7d26edeb8ca5b59e
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-01-12 22:37:22 +0100

    Now assigning link to images to post

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
-jQuery(document).ready(function() {
 
-	//Getting all images elements...
+	jQuery(document).ready(function() {
 
-	
+		if (is_single()) {
 
-});
+			var images = getAllImagesFromPost();
+
+			images.forEach(function(image) {
+				assignLink(image);
+			});
+
+		}
+
+	});
+
+
+	function getAllImagesFromPost() {
+
+		var images = [];
+
+		jQuery(".post").find("img").each(function(i) {
+			images.push(jQuery(this));
+		});
+
+		return images;
+	}
+
+	function assignLink(image) {
+
+		//getting media ID
+		var imageClasses = image.attr("class").split(' ');
+		var media_ID;
+		imageClasses.forEach(function(iClass) {
+			if(iClass.indexOf("wp-image-") > -1) {
+				var pieces = iClass.split('-');
+				media_ID = pieces[pieces.length - 1];
+			}
+		});
+
+		if (media_ID) {
+			var parent = image.parent();
+			parent.attr("href", get_site_url() + "?p=" + media_ID);
+		}
+	}
--- wp-image-post-transformer.php
+++ wp-image-post-transformer.php
@@ -10,10 +10,31 @@ Author URI: http://alexandrenguyen.fr
 License: MIT
 */
 
-add_action( 'wp_enqueue_scripts', 'register_scripts', 99999 );
+add_action( 'wp_enqueue_scripts', 'register_scripts', 9999 );
+add_action('wp_head', 'print_head');
 
 function register_scripts() {
-	    wp_register_script( 'load-images-script', plugins_url( '/js/script.js', __FILE__ ) );
+	wp_register_script( 'load-images-script', plugins_url( '/js/script.js', __FILE__ ) );
+	wp_enqueue_script('load-images-script');
+}
+
+function print_head() {
+	?>
+		<script>
+
+			if(!get_site_url) {
+				function get_site_url() {
+					return "<?php echo get_site_url(); ?>";
+				}
+			}		
+			if(!is_single) {
+				function is_single() {
+					return <?php echo is_single() == 1 ? "true" : "false"; ?>;
+				}
+			}
+
+		</script>
+	<?php
 }
 
 ?>
